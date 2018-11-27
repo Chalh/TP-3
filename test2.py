79d5a82322fb1c62a0635bed4cd03a487049e9a7
chiheb <chiheb.elouekdi@gmail.com>
2018-11-27 11:26:20 -0500

    fin

--- test2.py
+++ test2.py
@@ -28,6 +28,7 @@ from sklearn.linear_model import LogisticRegression
 from nltk import sent_tokenize
 from nltk import pos_tag
 from nltk.stem import PorterStemmer
+import sys
 
 ps = PorterStemmer()
 
@@ -118,34 +119,45 @@ data_test = data_test.reindex(np.random.permutation(data_test.index))
 Nb_col = 1000
 
 
-X_train = []
-Y_train = []
+DX_train = []
+DY_train = []
 #xyz = data_train.values[:Nb_col]
 xyz = data_train.values
 for i in xyz:
     phrase = " ".join(i[1:4])
-    X_train.append(phrase)
-    Y_train.append(i[4])
+    DX_train.append(phrase)
+    DY_train.append(i[4])
 
+DX_test = []
+
+#xyz = data_test.values[:Nb_col]
+xyz = data_test.values
+for i in xyz:
+    phrase = " ".join(i[1:4])
+    DX_test.append(phrase)
 
 
 print("ok1")
 mindf = 1
 documents = []
-for sen in X_train:
-#for sen in range(0, len(X_train)):
+for sen in DX_train:
     documents.append(Tranforme_texte(sen,3))
+
+documents_test = []
+for sen in DX_test:
+    documents_test.append(Tranforme_texte(sen,3))
 
 print("ok2")
 vectorizer = CountVectorizer(min_df=mindf, stop_words=stopwd)
 #vectorizer = TfidfVectorizer(min_df=mindf, stop_words=stopwd)
+
 X = vectorizer.fit_transform(documents)
-#X_train, X_test, y_train, y_test = train_test_split(X, Y_train, test_size=0.1, random_state=0)
+XT = vectorizer.fit_transform(documents_test)
+x_train, x_test, y_train, y_test = shuffle(X, DY_train, 4)
 
-X_train, X_test, y_train, y_test = shuffle(X, Y_train, 4)
-
-clf = MultinomialNB().fit(X_train, y_train)
-y_pred = clf.predict(X_test)
+clf = MultinomialNB().fit(x_train, y_train)
+y_pred = clf.predict(x_test)
+dy_pred = clf.predict(XT)
 
 # 3. fit
 #logreg.fit(X_train, y)
@@ -155,9 +167,17 @@ accuracy_score_nb = sklearn.metrics.accuracy_score(y_test, y_pred)
 #recall_nb = recall_score(y_test, y_pred)
 print(accuracy_score_nb)
 
-logreg = LogisticRegression().fit(X_train, y_train)
-y_pred = logreg.predict(X_test)
+logreg = LogisticRegression().fit(x_train, y_train)
+y_pred = logreg.predict(x_test)
 #average_precision_lr = average_precision_score(y_test, y_pred)
 #recall_lg = recall_score(y_test, y_pred)
 accuracy_score_lr = sklearn.metrics.accuracy_score(y_test, y_pred)
 print(accuracy_score_lr)
+
+f = open('Out','w')
+stdout_old = sys.stdout
+sys.stdout = f
+print("NAIVE BAYES")
+i=0
+for x in dy_pred:
+    print(DX_test[i].__str__()+";"+x)
